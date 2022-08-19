a66c27f277b6c69a48f2d6a86c121da270e09f4a
Sergio Álvarez <sergio@codecoolture.com>
2022-08-19 17:01:03 +0200

    Replace favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link rel="shortcut icon" href="/static/img/favicon-64.png" type="image/png" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>"
+          />
         </Head>
 
         <body className="dark-theme">
