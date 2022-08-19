22e2c0aa65d20ba94be82afe32061790dcbecec9
Sergio Álvarez <sergio@codecoolture.com>
2022-08-19 16:54:51 +0200

    Create a new basic homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
-import { Figure } from "../components/Figure";
+import { Link } from "../components/Link";
 import { Application } from "../layouts/Application";
 
 export default function Homepage() {
-  return <Application hideBackLink></Application>;
+  return (
+    <Application hideBackLink>
+      <section className="Homepage">
+        <p className="Homepage__Text">
+          Hi there! 👋 I&apos;m Sergio, a software engineer advocating for software development best practices and good
+          software design.
+        </p>
+
+        <p className="Homepage__Text">
+          I&apos;m still working on this website, but you&apos;re free to browse around and check out the{" "}
+          <Link href="./blog">blog</Link> and <Link href="./notes">notes</Link>!
+        </p>
+      </section>
+    </Application>
+  );
 }
