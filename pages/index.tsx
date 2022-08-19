c6b1e6cc6d804ab99c6c00ea4cf17eff17f3c69b
Sergio Álvarez <sergio@codecoolture.com>
2022-08-19 17:08:57 +0200

    Use Application__Column to fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,17 @@ export default function Homepage() {
   return (
     <Application hideBackLink>
       <section className="Homepage">
-        <p className="Homepage__Text">
-          Hi there! 👋 I&apos;m Sergio, a software engineer advocating for software development best practices and good
-          software design.
-        </p>
+        <div className="Application__Column">
+          <p className="Homepage__Text">
+            Hi there! 👋 I&apos;m Sergio, a software engineer advocating for software development best practices and
+            good software design.
+          </p>
 
-        <p className="Homepage__Text">
-          I&apos;m still working on this website, but you&apos;re free to browse around and check out the{" "}
-          <Link href="./blog">blog</Link> and <Link href="./notes">notes</Link>!
-        </p>
+          <p className="Homepage__Text">
+            I&apos;m still working on this website, but you&apos;re free to browse around and check out the{" "}
+            <Link href="./blog">blog</Link> and <Link href="./notes">notes</Link>!
+          </p>
+        </div>
       </section>
     </Application>
   );
