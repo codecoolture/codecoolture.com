38bf0417d5ffc83deb06d8d39e72e32166234a90
Sergio Álvarez <sergio@codecoolture.com>
2022-09-08 21:40:32 +0200

    Fix minor type error

--- layouts/Posts/index.tsx
+++ layouts/Posts/index.tsx
@@ -19,47 +19,51 @@ export function Posts({ posts }: PostsProps) {
     return acc;
   }, {});
 
-  return orderBy(Object.entries(groupByYear), "[0]", "desc").map(([year, articles]) => {
-    return (
-      <>
-        <Heading el="h2" size="xl" className="Posts__Title">
-          {year}
-        </Heading>
+  return (
+    <>
+      {orderBy(Object.entries(groupByYear), "[0]", "desc").map(([year, articles]) => {
+        return (
+          <>
+            <Heading el="h2" size="xl" className="Posts__Title">
+              {year}
+            </Heading>
 
-        {articles.map((post) => {
-          const { date, draft, language, spoiler, title, url } = post.metadata;
+            {articles.map((post) => {
+              const { date, draft, language, spoiler, title, url } = post.metadata;
 
-          return (
-            <>
-              <Heading el="p" size="m" className="Post__Title">
-                <NextLink href={url} passHref>
-                  <Link>{title}</Link>
-                </NextLink>
-              </Heading>
+              return (
+                <>
+                  <Heading el="p" size="m" className="Post__Title">
+                    <NextLink href={url} passHref>
+                      <Link>{title}</Link>
+                    </NextLink>
+                  </Heading>
 
-              <ul className="Post__Subheading">
-                <li className="Post__Subheading__Item">
-                  <Timestamp className="Post__Timestamp" date={date} />
-                </li>
+                  <ul className="Post__Subheading">
+                    <li className="Post__Subheading__Item">
+                      <Timestamp className="Post__Timestamp" date={date} />
+                    </li>
 
-                {typeof draft === "boolean" && draft && (
-                  <li className="Post__Subheading__Item">
-                    <p className="Post__Badge Post__Badge--Draft">Draft</p>
-                  </li>
-                )}
+                    {typeof draft === "boolean" && draft && (
+                      <li className="Post__Subheading__Item">
+                        <p className="Post__Badge Post__Badge--Draft">Draft</p>
+                      </li>
+                    )}
 
-                {typeof language === "string" && (
-                  <li className="Post__Subheading__Item">
-                    <p className="Post__Badge Post__Badge--Language">{language}</p>
-                  </li>
-                )}
-              </ul>
+                    {typeof language === "string" && (
+                      <li className="Post__Subheading__Item">
+                        <p className="Post__Badge Post__Badge--Language">{language}</p>
+                      </li>
+                    )}
+                  </ul>
 
-              <Text>{spoiler}</Text>
-            </>
-          );
-        })}
-      </>
-    );
-  });
+                  <Text>{spoiler}</Text>
+                </>
+              );
+            })}
+          </>
+        );
+      })}
+    </>
+  );
 }
