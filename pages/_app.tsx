44dd3fa2583b856cc063c3b8e246f335d02438d7
Sergio Álvarez <sergio@codecoolture.com>
2022-12-18 00:55:59 +0100

    Use @next/font instead of Google Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import { IBM_Plex_Mono, IBM_Plex_Sans, IBM_Plex_Serif } from "@next/font/google";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { Seo } from "../components/Seo";
 import { useFathom } from "../hooks";
 import "../styles/index.css";
+
+const ibmPlexMono = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400", "700"] });
+const ibmPlexSans = IBM_Plex_Sans({ subsets: ["latin"], style: ["italic", "normal"], weight: ["400", "700"] });
+const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], style: ["italic", "normal"], weight: ["400", "700"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useFathom();
@@ -14,6 +19,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       <Seo />
+
+      <style jsx global>{`
+        :root {
+          --font-ibm-plex-mono: ${ibmPlexMono.style.fontFamily};
+          --font-ibm-plex-sans: ${ibmPlexSans.style.fontFamily};
+          --font-ibm-plex-serif: ${ibmPlexSerif.style.fontFamily};
+        }
+      `}</style>
 
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,6 @@ export default class MyDocument extends Document {
 
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans:ital,wght@0,400;0,700;1,400;1,700&family=IBM+Plex+Serif:ital,wght@0,400;0,700;1,400&display=swap"
-            rel="stylesheet"
-          />
-
           <link
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>"
