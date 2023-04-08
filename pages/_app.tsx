a37e71daca6d23601895f4e8002b17a89f719e68
Sergio Álvarez <sergio@codecoolture.com>
2023-04-08 23:24:01 +0200

    Add Mastodon verification link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,6 +29,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link href="https://mastodon.world/@codecoolture" rel="me" />
       </Head>
 
       <Seo />
