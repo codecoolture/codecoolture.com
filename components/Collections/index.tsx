b0c408aa70ba9c7b783eeeb1513b286fb56fec82
Sergio Álvarez <sergio@codecoolture.com>
2023-07-17 09:49:25 +0200

    Change the shape of the collection link

--- components/Collections/index.tsx
+++ components/Collections/index.tsx
@@ -1,7 +1,6 @@
 import NextLink from "next/link";
 
 import { ApiCollection } from "@/cms/api/ApiCollection";
-import { Link } from "@/components/Link";
 
 export function Collections(props: { collections: ApiCollection[] }) {
   if (props.collections.length === 0) {
@@ -11,10 +10,10 @@ export function Collections(props: { collections: ApiCollection[] }) {
   return (
     <ul className="Collections">
       {props.collections.map((collection) => (
-        <li key={collection.slug} className="Collections__Collection">
-          <Link as={NextLink} href={`/collections/${collection.slug}`}>
+        <li key={collection.slug}>
+          <NextLink className="Collections__Collection" href={`/collections/${collection.slug}`}>
             #{collection.slug}
-          </Link>
+          </NextLink>
         </li>
       ))}
     </ul>
