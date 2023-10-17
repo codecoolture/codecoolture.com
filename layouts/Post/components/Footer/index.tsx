7232ffd2f433ce8a1dfd8a67076f75af13876db2
Sergio Álvarez <sergio@codecoolture.com>
2023-10-17 09:06:54 +0200

    Redesign the post footer

--- layouts/Post/components/Footer/index.tsx
+++ layouts/Post/components/Footer/index.tsx
@@ -1,41 +1,34 @@
-import { capitalize } from "lodash";
-import React from "react";
-import { FaGithub, FaXTwitter } from "react-icons/fa6";
-
 import { ApiArticle } from "@/cms/api/ApiArticle";
-
-type ChipProps = { children: React.ReactNode; href: string; type: "twitter" | "github" };
-
-const Chip = ({ children, href, type }: ChipProps) => {
-  const kype = capitalize(type);
-  const Icon = type === "twitter" ? FaXTwitter : FaGithub;
-
-  const classes = ["Post__Footer__Chip", `Post__Footer__Chip--${kype}`].join(" ");
-
-  return (
-    <a className={classes} href={href} target="blank">
-      <Icon className="Post__Footer__Chip__Icon" />
-      {children}
-    </a>
-  );
-};
 
 type FooterProps = { post: ApiArticle };
 
 export function Footer({ post }: FooterProps) {
-  const githubEditUrl = `https://github.com/codecoolture/codecoolture.com/edit/trunk/cms/content${post.url}.mdx`;
   const shareableUrl = `https://codecoolture.com${post.url}`;
   const tweet = encodeURIComponent(`Check out "${post.title}" by @codecoolture\n\n`);
 
   return (
     <footer className="Post__Footer">
-      <Chip type="twitter" href={`https://x.com/intent/tweet?url=${encodeURIComponent(shareableUrl)}&text=${tweet}`}>
-        Share on Twitter
-      </Chip>
+      <nav>
+        <ul className="Post__Footer__List">
+          <li className="Post__Footer__ListItem">
+            <a
+              className="Post__Footer__Link"
+              href={`https://x.com/intent/tweet?url=${encodeURIComponent(shareableUrl)}&text=${tweet}`}
+            >
+              Share
+            </a>
+          </li>
 
-      <Chip type="github" href={githubEditUrl}>
-        Edit on GitHub
-      </Chip>
+          <li className="Post__Footer__ListItem">
+            <a
+              className="Post__Footer__Link"
+              href={`https://github.com/codecoolture/codecoolture.com/edit/trunk/cms/content${post.url}.mdx`}
+            >
+              Edit on GitHub
+            </a>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 }
